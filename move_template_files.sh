41043305d37f1d4d7fb63dcc91e53527bde77b6a
JosePizarro3 <JosePizarro3@users.noreply.github.com>
2024-10-28 14:05:21 +0000

    chore: accept new Cruft update

--- move_template_files.sh
+++ move_template_files.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-rsync -avh nomad-parser-edmft/ .
-rm -rfv nomad-parser-edmft
+if ! command -v rsync >/dev/null 2>&1; then
+  echo "rsync required, but not installed!"
+  exit 1
+else
+  rsync -avh nomad-parser-edmft/ .
+  rm -rfv nomad-parser-edmft
+fi
